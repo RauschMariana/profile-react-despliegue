3deb55956ccd9233bed1744b74cfb3c2e2cf4a98
Mariana Rausch <marianarauschb@gmail.com>
2023-10-25 03:20:18 -0300

    fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <div className="container mt-5">
+        <h1>Bienvenido al Perfil de Usuario</h1>
+        <p>Esta es la página principal del perfil. Aquí puedes encontrar una breve introducción o bienvenida al perfil del usuario.</p>
+        </div>
     </>
   )
 };
